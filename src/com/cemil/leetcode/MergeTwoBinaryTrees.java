22d13e6c9c66f2a860784a4c6bb58dc9cf021d65
cemÄilcakmak <b.cemilcakmak@gmail.com>
2021-12-02 21:51:46 +0300

    Implemented a solution in a recursive way

---
+++ src/com/cemil/leetcode/MergeTwoBinaryTrees.java
@@ -0,0 +1,37 @@
+package com.cemil.leetcode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+
+        return root1;
+    }
+}
